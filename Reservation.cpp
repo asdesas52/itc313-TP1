14eac5c9fb212948a702a9f732093ef57acf6c91
Anass <mns@MacBook-Air-de-Anass.local>
2019-11-15 14:36:34 +0100

    Question 5: ok

---
+++ Reservation.cpp
@@ -0,0 +1,206 @@
+#include "Reservation.h"
+
+
+void Reservation::setIdR(int idR){
+	
+	m_id_r=idR;
+	
+	
+	
+}
+
+void Reservation::setIdC(int idC){
+	
+	
+	m_id_c=idC;
+	
+	
+}
+
+void Reservation::setIdCh(int idCh){
+	
+	
+	m_id_ch=idCh;
+	
+	
+}
+
+void Reservation::setIdH(int idH){
+	
+	
+	m_id_h=idH;
+	
+	
+}
+
+
+int Reservation::getIdR(){
+	
+	
+	return m_id_r;
+		
+}
+
+
+int Reservation::getIdH(){
+	
+	
+	return m_id_h;
+		
+}
+
+
+
+int Reservation::getIdCh(){
+	
+	
+	return m_id_ch;
+		
+}
+
+
+int Reservation::getIdC(){
+	
+	
+	return m_id_c;
+		
+}
+
+
+void Reservation::setRDate(int dayS, int monthS, int yearS, int dayE, int monthE, int yearE){
+	
+	if (yearS>=0 && yearE>=0 && yearS<=yearE){
+		if (monthS>=1 && monthE>=1 && monthS<=12 && monthE<=12 && monthS<=monthE){
+			if (dayS>=1 && dayE>=1 && dayS<=31 && dayE<=31 && dayS<=dayE){
+				
+				m_day_s=dayS;
+				m_day_e=dayE;
+				m_month_s=monthS;
+				m_month_e=monthE;
+				m_year_s=yearS;
+				m_year_e=yearE;
+			}
+			
+			else {
+				cerr << "Error on day ! " << endl; 
+				
+			}
+			
+		}
+		
+		else {
+			cerr << "Error on month ! " << endl; 
+			}
+		}
+		else {
+			cerr << "Error on year ! " << endl; 
+			}
+			
+	if (monthS==monthE && yearE==yearS){
+		m_night=dayE-dayS;
+	}
+	
+	else if (yearE==yearS){
+		m_night=31-dayS+(monthE-monthS+1)*31+dayE;
+	}
+	
+	else if (yearE=!yearS){
+		m_night=31-dayS+(12-monthS+monthE+1)*31+dayE;
+	}
+			
+			
+			
+			
+			
+			
+			
+	
+}
+
+
+void Reservation::getRDate(){
+	
+	cout << "Date Debut : " << m_day_s << " / " << m_month_s << " / " << m_year_s << endl;
+	cout << "Date Fin   : " << m_day_e << " / " << m_month_e << " / " << m_year_e << endl;
+	cout << "Nombre de nuits : " << m_night << endl;
+	
+	
+	
+	
+	
+	
+}
+
+void Reservation::setRPrice(float price, float discount){
+	
+	
+	
+	m_price_r=m_night*price*discount;
+	
+
+	
+	
+}
+
+
+
+float Reservation::getRPrice(){
+	
+	
+	return m_price_r;
+	
+}
+
+int Reservation::day_s(){
+	
+	return m_day_s;
+}
+
+int Reservation::month_s(){
+	
+	return m_month_s;
+}
+
+int Reservation::year_s(){
+	
+	return m_year_s;
+}
+
+
+int Reservation::day_e(){
+	
+	return m_day_e;
+}
+
+int Reservation::month_e(){
+	
+	return m_month_e;
+}
+
+int Reservation::year_e(){
+	
+	return m_year_e;
+}
+
+
+
+int Reservation::night(){
+	
+	return m_night;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Reservation.h
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+
+
+#ifndef RESERVATION_H
+#define RESERVATION_H
+
+class Reservation {
+	
+	private :
+		
+		
+		int m_id_r;
+		int m_id_h;
+		int m_id_ch;
+		int m_id_c;
+		
+		float m_price_r;
+		
+		int m_day_s;
+		int m_month_s;
+		int m_year_s;
+		
+		int m_day_e;
+		int m_month_e;
+		int m_year_e;
+		
+		int m_night=0;
+		
+		
+	public :
+	
+		
+		void setIdR(int idR);
+		int getIdR();
+		
+		void setIdH(int idH);
+		int getIdH();
+		
+		void setIdC(int idC);
+		int getIdC();
+		
+		void setIdCh(int idCh);
+		int getIdCh();
+		
+		
+		void setRPrice(float price, float discount);
+		float  getRPrice();
+		
+		
+		int day_s();
+		int month_s();
+		int year_s();
+		
+		
+		int day_e();
+		int month_e();
+		int year_e();
+		
+		int night();
+		
+		void setRDate(int dayS, int dayE, int monthS, int monthE, int yearS, int yearE);
+		void getRDate();
+		
+};
+
+#endif
