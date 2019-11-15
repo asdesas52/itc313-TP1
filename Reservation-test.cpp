9e073ac3100ebde69eae0d2a64a220c3afbdd496
Anass <mns@MacBook-Air-de-Anass.local>
2019-11-15 14:39:02 +0100

    Question 5 : ok

---
+++ Reservation-test.cpp
@@ -0,0 +1,33 @@
+#include "Reservation.h"
+
+int main(){
+	
+	
+	Reservation r;
+	
+	r.setIdH(91);
+	cout << "Id Hotel : " << r.getIdH() << endl;
+	
+	r.setIdR(1276576);
+	cout << "Id Reservation : " << r.getIdR() << endl;
+	
+	r.setIdC(18767556);
+	cout << "Id Client : " << r.getIdC() << endl;
+	
+	r.setIdCh(1001);
+	cout << "Chambre n° : " << r.getIdCh() << endl;
+	
+	r.setRDate(1,2,19,4,5,19);
+
+	r.getRDate();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;	
+}
